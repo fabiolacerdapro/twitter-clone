f4904845e1f592f68a108b86b2a13933e3a1c73e
Fabio A. Lacerda <34407504+fabiolacerdapro@users.noreply.github.com>
2023-08-13 11:39:42 +0000

    Mudando metadatas do OG na pasta `common`

--- src/components/common/app-head.tsx
+++ src/components/common/app-head.tsx
@@ -3,12 +3,12 @@ import Head from 'next/head';
 export function AppHead(): JSX.Element {
   return (
     <Head>
-      <title>Twitter</title>
-      <meta name='og:title' content='Twitter' />
-      <link rel='icon' href='/favicon.ico' />
+      <title>Network</title>
+      <meta name='og:title' content='Network' />
+      <link rel='icon' href='/latter-n.png' />
       <link rel='manifest' href='/site.webmanifest' key='site-manifest' />
-      <meta name='twitter:site' content='@ccrsxx' />
-      <meta name='twitter:card' content='summary_large_image' />
+      <meta name='network:site' content='@ccrsxx' />
+      <meta name='network:card' content='' />
     </Head>
   );
 }
